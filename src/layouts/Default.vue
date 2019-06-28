c162578d88b0c0103b3fbf7ba8f8b96e155e0a62
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2019-06-28 15:25:14 +0200

    WebFontLoader

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -19,7 +19,7 @@ query {
 <script>
 import Header from '~/components/Header.vue'
 import Sidebar from '~/components/Sidebar.vue'
-import throttle from 'lodash/throttle'
+import WebFontLoader from 'webfontloader'
 
 export default {
   components: {
@@ -35,6 +35,13 @@ export default {
           .then(function() { console.log("Service Worker Registered"); });
       }
     }
+  },
+  created() {
+    WebFontLoader.load({
+      google: {
+        families: ['Source+Sans+Pro:400,400i,700&display=swap']
+      }
+    });
   }
 }
 </script>
