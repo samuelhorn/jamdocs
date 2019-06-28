0882912115323dc925efb914c75f8eae2952be13
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2019-06-28 10:47:17 +0200

    Added meta description to docs

--- src/templates/Doc.vue
+++ src/templates/Doc.vue
@@ -25,7 +25,9 @@ export default {
   metaInfo() {
     return {
       title: this.$page.doc.title,
-      description: this.$page.doc.description
+      meta: [
+        { key: 'description', name: 'description', content: this.$page.doc.description }
+      ]
     }
   }
 }
