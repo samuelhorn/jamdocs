6723eb70e845db3fa8a0dc342ec8b69ad84981be
Samuel Horn af Rantzien <samuel@samuelhorn.com>
2019-07-01 20:53:25 +0200

    Added analytics plugin

--- gridsome.config.js
+++ gridsome.config.js
@@ -30,6 +30,12 @@ module.exports = {
           ]
         }
       }
+    },
+    {
+      use: '@gridsome/plugin-google-analytics',
+      options: {
+        id: process.env.GA_ID
+      }
     }
   ],
   chainWebpack: config => {
